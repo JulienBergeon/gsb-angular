6c29f8004bd14edaa8b631db28c486e66a03c1be
Badji Lounes <lounes.badji@cpe.fr>
2020-03-17 16:01:51 +0100

    feature pagination - fix bug pagination on search - fix bug pagination on page size change

--- src/app/components/medicines/medicines.component.ts
+++ src/app/components/medicines/medicines.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormGroup } from '@angular/forms';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { ApiService } from 'src/app/services/api/api.service';
-import { ListService } from 'src/app/services/list/list.service';
 import { MedicinesInterface } from 'src/model/medicines.interface';
 
 
@@ -13,20 +11,21 @@ import { MedicinesInterface } from 'src/model/medicines.interface';
   styleUrls: ['./medicines.component.scss']
 })
 export class MedicinesComponent implements OnInit {
-
   search = '';
   medicines: MedicinesInterface[] = [];
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  pageIndex = 0;
+  pageSize = 5;
 
   constructor(private readonly api: ApiService) {}
 
   ngOnInit() {
-    this.getMedicines(0, 5, this.search);
+    this.getMedicines(this.search);
   }
 
 
-  getMedicines(pageIndex: number, pageSize: number, search: string): void {
-    this.api.get('medicines', {pageIndex, pageSize, search}).toPromise().then(
+  getMedicines(search: string): void {
+    this.api.get('medicines', {pageIndex: this.pageIndex, pageSize: this.pageSize, search}).toPromise().then(
       paginatedMedicines => {
         this.medicines = paginatedMedicines.elements;
         this.paginator.length = paginatedMedicines.nbElements;
@@ -34,16 +33,31 @@ export class MedicinesComponent implements OnInit {
       error => console.log(error)
     );
   }
-  
+
   pageChange(event: PageEvent): void {
-    // TODO handle pageSize change only
-    this.getMedicines(event.pageIndex, event.pageSize, this.search);
+    if (event.pageSize !== this.pageSize) {
+      this.paginator.firstPage();
+    }
+
+    this.pageIndex = this.paginator.pageIndex;
+    this.pageSize = this.paginator.pageSize;
+
+
+    this.getMedicines(this.search);
   }
 
-  delete(id){
-    console.log(id);
-    console.log('medicines/'+id);
-    
-    this.api.delete('medicines/'+id, id).toPromise().then(sucess => console.log(sucess), error=> console.log(error));
+  searchChanged(search: string) {
+    this.paginator.firstPage();
+    this.search = search;
+    this.getMedicines(this.search);
+  }
+
+  delete(id: number): void {
+    this.api.delete(`medicines/${id}`, id).toPromise().then(
+      () => {
+        this.getMedicines(this.search);
+      },
+      error => console.log(error)
+    );
   }
 }
